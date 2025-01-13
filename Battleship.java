93cad479fc210116a9ddb140b7192d257b85a6e1
Albrecht <l.albrecht@rheinenergie.com>
2025-01-13 12:28:08 +0100

    surrounding coordinates

--- Battleship.java
+++ Battleship.java
@@ -6,4 +6,19 @@ public class Battleship{
     static Coordinate getRandomCoordinate() {
         return new Coordinate(Utility.getRandomInt(SIZE),Utility.getRandomInt(SIZE));
     }
+    static int getMaxSurroundingColumn(final Coordinate start, final Coordinate end){
+        return Math.min((Math.max(start.column(),end.column())+1),SIZE - 1);
+    }
+
+    static int getMaxSurroundingRow(final Coordinate start, final Coordinate end){
+        return Math.min((Math.max(start.row(),end.row())+1),SIZE - 1);
+    }
+
+    static int getMinSurroundingColumn(final Coordinate start, final Coordinate end){
+        return Math.max((Math.min(start.column(),end.column())-1),0);
+    }
+
+    static int getMinSurroundingRow(final Coordinate start, final Coordinate end){
+        return Math.max((Math.min(start.row(),end.row())-1),0);
+    }
 }
