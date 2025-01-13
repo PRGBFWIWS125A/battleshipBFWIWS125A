585abd2603275c4f81dbd2e72f60e08fd12fc416
LoetGurke <118271293+LoetGurke@users.noreply.github.com>
2025-01-13 11:16:57 +0100

    updated Field.java

--- Field.java
+++ Field.java
@@ -3,6 +3,10 @@ public class Field {
     String field;
 
     public Field(String field) {
+        setField(field);
+    }
+
+    public String setField(String Field) {
         if (field.equals("WATER") || field.equals("SHIP") || field.equals("SHIP_HIT") || field.equals("WATER_HIT")) {
             this.field = field;
         } else {
